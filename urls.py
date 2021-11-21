b676e50c57e59374ba2607f4e534db4a4170e24a
MoonPengu <63443127+MoonPengu@users.noreply.github.com>
2021-11-21 12:33:14 +0530

    Fetch all the urls and save in csv file

---
+++ urls.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+urls = input('Type out the url : ')
+grab = requests.get(urls)
+soup = BeautifulSoup(grab.text, 'html.parser')
+
+# opening a file in write mode
+f = open("test1.txt", "w")
+# traverse paragraphs from soup
+for link in soup.find_all("a"):
+    data = link.get('href')
+    f.write(data)
+    f.write("\n")
+
+f.close()
